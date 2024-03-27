c76d117ec946e168e361f15b7805ea28ec41df12
feelmystarshine <usmanadekojo@gmail.com>
2024-03-27 11:26:49 +0100

    second commit

--- Project/settings.py
+++ Project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i4o&dji*ucxh6s3@fktx&!)g4t5vjany!&g_x!ghx-#n%bdqs2'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast = bool)
 
 ALLOWED_HOSTS = []
 
